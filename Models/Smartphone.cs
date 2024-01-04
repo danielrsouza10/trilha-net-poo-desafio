3139ef89292f0332ef73716dc8b939ef960f8950
Daniel Souza <danielrsouza10@gmail.com>
2024-01-04 18:36:32 -0300

    desafio concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,8 +2,14 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+        public string Numero { get; set; }
+        public string Modelo { get => _modelo; set => _modelo = value; }
+        public string Imei { get => _imei; set => _imei = value; }
+        public int Memoria { get => _memoria; set => _memoria = value; }
 
         public Smartphone(string numero)
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("(62) 8888-7777");
+Console.WriteLine(nokia.Numero);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.Modelo = "1100";
+Console.WriteLine(nokia.Modelo);
+
+Console.WriteLine("-----------------------------");
+
+Iphone iphone = new Iphone("(63) 9999-8888");
+Console.WriteLine(iphone.Numero);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("youtube");
+iphone.Modelo = "13 Pro Max";
+Console.WriteLine(iphone.Modelo);
